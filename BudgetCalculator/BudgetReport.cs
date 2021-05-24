3eebbc2fd2fe56d213d53137e97fd613dbab9660
Sammy Wong <66samwon@gafe.molndal.se>
2021-05-24 14:05:16 +0200

    Keep working

--- BudgetCalculator/BudgetReport.cs
+++ BudgetCalculator/BudgetReport.cs
@@ -47,11 +47,13 @@ namespace BudgetCalculator
         {
             BudgetReport report = new BudgetReport(ecoController);
             string reportString = null;
-            reportString = $"Total Income:   {report.TotalIncome}\n" + 
-                           $"Total Expenses: {report.TotalExpenses}\n" +
-                           $"Total Saving:   {report.TotalMoneyForSavings}\n" +
-                           $"Cash:           {report.Balance}\n" +
-                           $"Expenses paid:   ";
+            reportString = $"Total Income:       {report.TotalIncome}\n" + 
+                           $"Total Expenses:     {report.TotalExpenses}\n" +
+                           $"Total Saving:       {report.TotalMoneyForSavings}\n" +
+                           $"Cash:               {report.Balance}\n" +
+                           $"Expenses (paid):    {UnWrapExpenses(report.PaidExpenses)}\n" +
+                           $"Expenses (unpaid):  {UnWrapExpenses(report.UnpaidExpenses)}\n" +
+                           $"Savings (paid);     {}";
             return reportString;
         }
 
