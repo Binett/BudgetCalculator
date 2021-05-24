8f10e80310773f1dcc92e516077c2a25b611a428
Sammy Wong <66samwon@gafe.molndal.se>
2021-05-24 16:07:22 +0200

    Create test for GetCalculatedDataToString()

---
+++ BudgetCalculatorTests1/BudgetReportTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BudgetCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetCalculator.Tests
+{
+    [TestClass()]
+    public class BudgetReportTests
+    {
+        [TestMethod()]
+        public void GetCalculatedDataToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
