42b7fdacbab26fa8799f55576aa5bf8fa835d8b7
Oskar Kling <oskarkling@gmail.com>
2021-05-25 15:04:26 +0200

    added tests for errorlogger

---
+++ BudgetCalculatorTests1/Helpers/ErrorLoggerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BudgetCalculator.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetCalculator.Helpers.Tests
+{
+    [TestClass()]
+    public class ErrorLoggerTests
+    {
+        [TestMethod()]
+        public void GetSummarizedLogAsStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
