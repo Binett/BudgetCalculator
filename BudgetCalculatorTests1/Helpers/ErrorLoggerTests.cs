ea245bd4718c7def23068c33131d81fdf956b8f0
Oskar Kling <oskarkling@gmail.com>
2021-05-25 15:44:35 +0200

    Finished errorlogtests

--- BudgetCalculatorTests1/Helpers/ErrorLoggerTests.cs
+++ BudgetCalculatorTests1/Helpers/ErrorLoggerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BudgetCalculatorTests1.Seeder;
+using System;
 
 namespace BudgetCalculator.Helpers.Tests
 {
@@ -28,17 +29,28 @@ namespace BudgetCalculator.Helpers.Tests
         }
 
         [TestMethod()]
-        public void AddTest()
+        public void GetSummarizedLogAsStringTest_UpdateEconomicObjectWithEmptyName_ShouldReturnLogMessageStringWasEmpty()
+        {
+            seeder.InitList();
+            seeder.ecoController.UpdateEconomicObjectName("Rent", "");
+            calc = new Calculator(seeder.ecoController);
+            BudgetReport report = new BudgetReport(seeder.ecoController);
+
+            var expected = "1: " + DateTime.Now + " BudgetCalculator.Controllers.EconomicController String name was empty\n";
+            var actual = ErrorLogger.GetSummarizedLogAsString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void AddTest_AddEconomicObjectWithDoubleMaxValue_ShouldReturnAnErrorInLogList()
         {
             seeder.InitList();
             seeder.ecoController.UpdateEconomicObjectAmount("Rent", double.MaxValue);
             calc = new Calculator(seeder.ecoController);
             BudgetReport report = new BudgetReport(seeder.ecoController);
 
-
-            var expected = "No Logs";
-            var actual = ErrorLogger.GetSummarizedLogAsString();
-
+            var expected = "BudgetCalculator.Controllers.EconomicController String name was empty";
+            var actual = ErrorLogger.GetLogList()[0];
             Assert.AreEqual(expected, actual);
         }
     }
